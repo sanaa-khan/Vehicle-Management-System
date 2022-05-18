4d1fcebd4505ffefff22064e078d793d30f74a9a
Sana Khan <i180439@nu.edu.pk>
2022-05-18 20:57:10 +0500

    Added update component and html

--- src/app/vehicle-dialog/vehicle-update-dialog/vehicle-update-dialog.component.ts
+++ src/app/vehicle-dialog/vehicle-update-dialog/vehicle-update-dialog.component.ts
@@ -10,19 +10,24 @@ import { VEHICLE } from '../../app.component';
 export class VehicleUpdateDialogComponent implements OnInit {
 
   public vehicle: VEHICLE;
+  public attributeToUpdate: string;
+  public valueToUpdate: any;
 
   constructor(
     private dialogRef: MatDialogRef<VehicleUpdateDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data: any) {
 
-    this.vehicle = data.vehicle
+    this.vehicle = data.vehicle;
+    this.attributeToUpdate = "";
+    this.valueToUpdate = "";
   }
 
   ngOnInit(): void {
     this.dialogRef.updateSize("570px");
   }
 
-  close() {
+  update() {
+    console.log(this.attributeToUpdate, this.valueToUpdate);
     this.dialogRef.close();
   }
 
